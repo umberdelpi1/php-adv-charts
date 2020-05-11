13f4d8483c67f704102fe5175fc3923e54843b5d
Umberto Del Piano <umberdelpi1@gmail.com>
2020-05-11 17:22:52 +0200

    Milestone1 end

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>php-adv-charts</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2/dist/Chart.min.js"></script>
+    <script src="main.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>Prova - Ex - Grafico</h1>
+          <canvas id="line"></canvas>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
